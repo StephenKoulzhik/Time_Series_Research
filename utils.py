49f82d83b6f36e96d1922e0f7d77f78a28f7a928
Stepan Kulzhik <smkulzhik@edu.hse.ru>
2024-04-27 18:40:59 +0300

    Repo organised a bit better

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from itertools import combinations, product, groupby
 
 
 class Lorentz:
-    def __init__(self, s=10, r=28, b=8/3):
+    def __init__(self, s=10.0, r=28.0, b=8 / 3):
         self.s = s
         self.r = r
         self.b = b
@@ -28,45 +28,33 @@ class Lorentz:
         l_1 = self.Y(x, y, z, r)
         m_1 = self.Z(x, y, z, b)
 
-        k_2 = self.X(
-            (x + k_1 * dt * 0.5),
-            (y + l_1 * dt * 0.5),
-            s
-        )
+        k_2 = self.X((x + k_1 * dt * 0.5), (y + l_1 * dt * 0.5), s)
 
         l_2 = self.Y(
-            (x + k_1 * dt * 0.5),
-            (y + l_1 * dt * 0.5),
-            (z + m_1 * dt * 0.5),
-            r
+            (x + k_1 * dt * 0.5), (y + l_1 * dt * 0.5), (z + m_1 * dt * 0.5), r
         )
 
         m_2 = self.Z(
-            (x + k_1 * dt * 0.5),
-            (y + l_1 * dt * 0.5),
-            (z + m_1 * dt * 0.5),
-            b
+            (x + k_1 * dt * 0.5), (y + l_1 * dt * 0.5), (z + m_1 * dt * 0.5), b
         )
 
-        k_3 = self.X((x + k_2 * dt * 0.5),
-                     (y + l_2 * dt * 0.5),
-                     s)
+        k_3 = self.X((x + k_2 * dt * 0.5), (y + l_2 * dt * 0.5), s)
 
-        l_3 = self.Y((x + k_2 * dt * 0.5),
-                     (y + l_2 * dt * 0.5),
-                     (z + m_2 * dt * 0.5), r)
+        l_3 = self.Y(
+            (x + k_2 * dt * 0.5), (y + l_2 * dt * 0.5), (z + m_2 * dt * 0.5), r
+        )
 
-        m_3 = self.Z((x + k_2 * dt * 0.5),
-                     (y + l_2 * dt * 0.5),
-                     (z + m_2 * dt * 0.5), b)
+        m_3 = self.Z(
+            (x + k_2 * dt * 0.5), (y + l_2 * dt * 0.5), (z + m_2 * dt * 0.5), b
+        )
 
         k_4 = self.X((x + k_3 * dt), (y + l_3 * dt), s)
         l_4 = self.Y((x + k_3 * dt), (y + l_3 * dt), (z + m_3 * dt), r)
         m_4 = self.Z((x + k_3 * dt), (y + l_3 * dt), (z + m_3 * dt), b)
 
-        x += (k_1 + 2 * k_2 + 2 * k_3 + k_4) * dt * (1/6)
-        y += (l_1 + 2 * l_2 + 2 * l_3 + l_4) * dt * (1/6)
-        z += (m_1 + 2 * m_2 + 2 * m_3 + m_4) * dt * (1/6)
+        x += (k_1 + 2 * k_2 + 2 * k_3 + k_4) * dt * (1 / 6)
+        y += (l_1 + 2 * l_2 + 2 * l_3 + l_4) * dt * (1 / 6)
+        z += (m_1 + 2 * m_2 + 2 * m_3 + m_4) * dt * (1 / 6)
 
         return (x, y, z)
 
@@ -138,7 +126,7 @@ def nth_element(dist, order, k):
 
 
 def volume(r, m):
-    return np.pi ** (m / 2) * r ** m / gamma(m / 2 + 1)
+    return np.pi ** (m / 2) * r**m / gamma(m / 2 + 1)
 
 
 def significant(cluster, h, p):
@@ -195,12 +183,14 @@ def get_clustering(x, k, h, verbose=True):
             if all(completed[wj] for wj in neigh_w):
                 w[i] = 0
                 continue
-            significant_clusters = set(wj for wj in neigh_w if significant(clusters[wj], h, p))
+            significant_clusters = set(
+                wj for wj in neigh_w if significant(clusters[wj], h, p)
+            )
             if len(significant_clusters) > 1:
                 w[i] = 0
                 for wj in neigh_w:
                     if wj in significant_clusters:
-                        completed[wj] = (wj != 0)
+                        completed[wj] = wj != 0
                     else:
                         for j in clusters[wj]:
                             w[j] = 0
@@ -237,35 +227,44 @@ def generate_centers(x_trains, N, WISHART_K=4, WISHART_H=0.2):
 
 
 class SimpleDeamon(object):
-    def __init__(self, mode='simple'):
+    def __init__(self, mode="simple"):
         self.mode = mode
-        self.predictions = {point: [None for i in range(point + 1)] for point in range(PTS)}
-        self.set_predictions = {point: [None for i in range(point + 1)] for point in range(PTS)}
+        self.predictions = {
+            point: [None for i in range(point + 1)] for point in range(PTS)
+        }
+        self.set_predictions = {
+            point: [None for i in range(point + 1)] for point in range(PTS)
+        }
 
         self.predicted = False
-        
+
     @property
     def label(self):
-        return 'Simple model of demon with ' + self.mode + ' mode'
+        return "Simple model of demon with " + self.mode + " mode"
+
+    def simple_aggr(pts):
+        if not pts:
+            return None
+        return np.mean(np.array(list(map(lambda center: center[0], pts))))
 
     def predict(self, start_point, step, preds):
         self.set_predictions[start_point].append(preds)
 
-        if self.mode == 'simple':
+        if self.mode == "simple":
             pred = simple_aggr(preds)
-        elif self.mode == 'd_weighted':
+        elif self.mode == "d_weighted":
             pred = aggr_d(preds)
-        elif self.mode == 'q_weighted':
+        elif self.mode == "q_weighted":
             pred = aggr_q(preds)
-        elif self.mode == 'mix':
+        elif self.mode == "mix":
             pred = aggr_mix(preds)
 
         self.predictions[start_point].append(pred)
         return pred
-    
+
     def get_predictions(self):
         return self.predictions
-    
+
     def get_set_predictions(self):
         return self.set_predictions
 
@@ -273,13 +272,20 @@ class SimpleDeamon(object):
         return self.predicted
 
 
-def generate_predictions(centers, deamon=None, return_set_pred=False,
-                         real_mode='test', EPS=0.05, Q_VALUE=0.99, PTS=100,
-                         STEPS=50):
+def generate_predictions(
+    centers,
+    deamon=None,
+    return_set_pred=False,
+    real_mode="test",
+    EPS=0.05,
+    Q_VALUE=0.99,
+    PTS=100,
+    STEPS=50,
+):
     preds = {}
     set_preds = {}
 
-    if real_mode == 'test':
+    if real_mode == "test":
         end_point = val_end
         init_point = test_init
     else:
@@ -293,15 +299,20 @@ def generate_predictions(centers, deamon=None, return_set_pred=False,
             set_preds[start_point] = [None] * (start_point + 1)
 
         # current window
-        wind = list(map(lambda x: (x, 1), xs[end_point + start_point : init_point + start_point]))
+        wind = list(
+            map(
+                lambda x: (x, 1), xs[end_point + start_point : init_point + start_point]
+            )
+        )
 
         for step in range(1, STEPS + 1):
             x_tests_for_point = {}
             for pattern in patterns:
 
-                key = str_subseq(pattern + (WINDOW - 1,)) 
-                sample = gen_sample_in_point_with_q(np.concatenate([wind, [(0, 0)]]), 
-                                                    WINDOW, pattern, len(wind))
+                key = str_subseq(pattern + (WINDOW - 1,))
+                sample = gen_sample_in_point_with_q(
+                    np.concatenate([wind, [(0, 0)]]), WINDOW, pattern, len(wind)
+                )
                 if not sample:
                     x_tests_for_point[key] = None
                 else:
@@ -321,10 +332,11 @@ def generate_predictions(centers, deamon=None, return_set_pred=False,
                         weight_q = np.mean(q_values) * Q_VALUE
                         chosen_centers.append((pattern, center, weight_d, weight_q))
 
-            last_points = list(map(lambda center: (
-                center[1][-1], center[2],
-                center[3], center[0]),
-                chosen_centers)
+            last_points = list(
+                map(
+                    lambda center: (center[1][-1], center[2], center[3], center[0]),
+                    chosen_centers,
+                )
             )
 
             # deamon predict
@@ -335,8 +347,9 @@ def generate_predictions(centers, deamon=None, return_set_pred=False,
                 set_preds[start_point].append(last_points)
 
             if result_point:
-                q_value = np.mean(np.array(list(map(lambda center: center[2], 
-                                  chosen_centers))))
+                q_value = np.mean(
+                    np.array(list(map(lambda center: center[2], chosen_centers)))
+                )
             else:
                 q_value = None
 
